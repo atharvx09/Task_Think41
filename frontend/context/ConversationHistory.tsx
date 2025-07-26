bb51734990f6e451ea58b1aee0d178e7056c690a
atharvx09 <113002246+atharvx09@users.noreply.github.com>
2025-07-26 10:50:18 +0530

    Create ConversationHistory.tsx

---
+++ frontend/context/ConversationHistory.tsx
@@ -0,0 +1,20 @@
+import { useChat } from "../context/ChatContext";
+
+const ConversationHistory = () => {
+  const { conversations, loadConversation } = useChat();
+
+  return (
+    <div className="conversation-history">
+      <h3>Past Conversations</h3>
+      <ul>
+        {conversations.map((conv) => (
+          <li key={conv._id} onClick={() => loadConversation(conv._id)}>
+            🗨️ {new Date(conv.created_at).toLocaleString()}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ConversationHistory;
